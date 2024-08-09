80bed212cae5ebf060acf75c88433f9c72137506
EunicePobee <arabaawilba@gmail.com>
2024-08-09 08:33:30 +0100

    Deployment (#39)
    * Created roles and assigned permissions
    
    * Further changes for deployment
    
    * More changes for api deployment
    
    * delete dist folder
    
    * add dist to gitignore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ import config from './config/config'
 dotenv.config();
 
 const NAMESPACE = 'Server';
-const router = express();
+const app = express();
 
 
 // Connecting to mongodb
@@ -26,7 +26,7 @@ mongoose.connect(config.mongo.url, config.mongo.options)
   });
 
 // Log the request
-router.use((req, res, next) => {
+app.use((req, res, next) => {
    logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
 
    res.on('finish', () => {
@@ -38,12 +38,12 @@ router.use((req, res, next) => {
 });
 
 //Parse the body of the request 
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 // Rules of the API
-router.use((req, res, next) => {
+app.use((req, res, next) => {
    // Set CORS headers
    res.header('Access-Control-Allow-Origin', '*');
    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
@@ -61,25 +61,31 @@ router.use((req, res, next) => {
 
 // Use Route
 
-router.use('/users',userRoutes)
-router.use('/hazard',hazardRoutes)
+app.use('/users',userRoutes)
+app.use('/hazard',hazardRoutes)
 
-router.use('/api', userRoutes)
+app.use('/api', userRoutes)
 
 
 // Error handling for not found routes
-router.use((req, res, next) =>{
+app.use((req, res, next) =>{
    const error = new Error('Not found');
 
 })
 
-const httpServer = http.createServer(router);
+// const httpServer = http.createServer(router);
 
-httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`));
+// httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`));
+
+// Listen for incoming requests
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
 
    
 // Error handling middleware
-router.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   logging.error(NAMESPACE, error.message, error);
   return res.status(500).json({
     message: error.message
