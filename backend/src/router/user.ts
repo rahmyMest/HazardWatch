0567d2d38874a2bcaa31acf36b2d0a4ef0dd1a5a
Afia Ofori <oforiafia1@gmail.com>
2026-03-27 16:02:36 +0000

    cleaned the router/user.ts and crate endpoints for users to retrieve the hazards they reported

--- backend/src/router/user.ts
+++ backend/src/router/user.ts
@@ -1,22 +1,43 @@
-import express from 'express';
-import controller from '../controllers/user';
-import {checkAuth, hasPermission } from '../middlewares/auth';
-
+import express from "express";
+import controller from "../controllers/user";
+import { checkAuth, hasPermission } from "../middlewares/auth";
 
 const router = express.Router();
 
-// Define routes
-router.post('/users/register', controller.register);
-router.post('/users/login', controller.login);
-router.post('/admin/signup', controller.adminSignup);
-router.post('/admin/signin', controller.adminSignin);
-router.patch('/users/', checkAuth, hasPermission('create_user'), controller.createUser);
-router.patch('/users/:id', checkAuth, hasPermission('update_user'), controller.editUser);
-router.delete('/users/:id', checkAuth, hasPermission('delete_user'), controller.deleteUser);
-router.post('/users/logout', checkAuth, controller.logout);
-router.get('/users', checkAuth, hasPermission('read_users'), controller.getAllUsers);
-router.get('/admin/reports',checkAuth,hasPermission('view_reports'),controller.getAllReports);
-
+// Define user routes
+router.post("/users/register", controller.register);
+router.post("/users/login", controller.login);
+router.patch(
+  "/users/",
+  checkAuth,
+  hasPermission("create_user"),
+  controller.createUser,
+);
+router.patch(
+  "/users/:id",
+  checkAuth,
+  hasPermission("update_user"),
+  controller.editUser,
+);
+router.delete(
+  "/users/:id",
+  checkAuth,
+  hasPermission("delete_user"),
+  controller.deleteUser,
+);
+router.post("/users/logout", checkAuth, controller.logout);
+router.get(
+  "/users",
+  checkAuth,
+  hasPermission("read_users"),
+  controller.getAllUsers,
+);
+router.get(
+  "/admin/reports",
+  checkAuth,
+  hasPermission("view_reports"),
+  controller.getAllReports,
+);
 
 // Export router
 export default router;
